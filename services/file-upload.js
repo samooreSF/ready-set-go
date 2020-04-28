471efcf8bf470813a87ef1a47e127177d953f45d
Satchel Moore <samoore@davidson.edu>
2020-04-28 16:33:39 -0500

    services

---
+++ services/file-upload.js
@@ -0,0 +1,34 @@
+const aws = require('aws-sdk');
+const multer = require('multer');
+const multerS3 = require('multer-s3');
+
+aws.config.update({
+  secretAccessKey: 'E0diJVpbEWMBK+xumPXoJXVQYa/pffYMj8//AYAh',
+  accessKeyId: 'AKIAJZA7AM7G5THCEIMQ',
+  region:'us-east-1',
+})
+const s3 = new aws.S3()
+
+const fileFilter = (req,file,cb) =>{
+  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png' || file.mimetype === 'video/mp4' || file.mimetype ===  "video/quicktime" || file.mimetype === "video/MP2T") {
+    cb(null,true)
+  }
+  else {
+    cb(new Error('invalid mime type, only JPEG and PNG'), false);
+  }
+}
+const  upload = multer({
+  fileFilter: fileFilter,
+  storage: multerS3({
+    s3: s3,
+    bucket: 'ready-set-go-app',
+    acl:"public-read",
+    metadata: function (req, file, cb) {
+      cb(null, {fieldName: "TESTING_META_DATA"});
+    },
+    key: function (req, file, cb) {
+      cb(null, Date.now().toString())
+    }
+  })
+})
+module.exports = upload;
